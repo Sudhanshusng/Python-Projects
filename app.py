47db2552504a14c997fe9130d42aa81e72514bdf
Sudhanshu Sanganeria <122626095+Sudhanshusng@users.noreply.github.com>
2025-03-01 17:24:38 +0530

    Update app.py

--- app.py
+++ app.py
@@ -9,15 +9,17 @@ def main():
     try:
         st.title("Sentiment Analyser")
         st.markdown(''' This app analyses your feelings based on the input text given by the user''')
-        input = st.text_area("Enter your input Text")
+        input = st.text_area("Enter your input Text" , height=200)
         analyse_button = st.button("Analyse Text")
-        if input is not None :
-         if analyse_button:
+        if analyse_button:
+         if input.strip() :
             sentiment_analyser = load_sentiment_analyser()
             result = sentiment_analyser(input)
             st.write(result)
-        else :
-            st.error("Enter correct Text")
+        
+         else:
+           st.error("INVALID TEXT")
+    
     except Exception as e:
         st.error(e)
 
